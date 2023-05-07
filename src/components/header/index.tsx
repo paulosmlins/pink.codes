df07990d034df4271ad0efd2fcef51b3aa856993
Paulo Lins <paulosilas02@hotmail.com>
2023-05-07 20:35:25 -0300

    Feat: Melhorando menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,29 +6,31 @@ import { faGear } from "@fortawesome/free-solid-svg-icons";
 export default function Home() {
   return (
     <header className={styles.header}>
-      <div>
-        <a
-          className={styles.headerLogo}
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/logo.svg"
-            alt="Logo Pink Codes"
-            width="45"
-            height="45"
-            priority
-          />
-        </a>
-      </div>
-      <div className={styles.headerMenu}>
-        <nav className={`flex gap-x-6 ${styles.navMenu}`}>
-          <a href="#">Home</a>
-          <a href="#">Blog</a>
-          <a href="#">Work</a>
-          <a href="#">About</a>
-        </nav>
+      <div className="flex gap-6 items-center">
+        <div>
+          <a
+            className={styles.headerLogo}
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/logo.svg"
+              alt="Logo Pink Codes"
+              width="45"
+              height="45"
+              priority
+            />
+          </a>
+        </div>
+        <div className={styles.headerMenu}>
+          <nav className={`flex gap-x-6 ${styles.navMenu}`}>
+            <a href="#">Inicio</a>
+            <a href="#">Lives</a>
+            <a href="#">Projetos</a>
+            <a href="#">Sobre Mim</a>
+          </nav>
+        </div>
       </div>
       <div className={styles.configBtn}>
         <FaIcon icon={faGear} />
