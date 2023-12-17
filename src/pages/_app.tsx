996c94ec485611b222a81a57f72b88d198bb2c5e
Paulo Lins <paulosmlins@192.168.100.7>
2023-12-17 19:42:58 -0300

    Feat: Tentando exibir background image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,10 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </div>
       </div>
+      <div className="pointer-events-none relative isolate -z-1 opacity-30">
+        <div className="fixed inset-0 mix-blend-color-dodge nnnoise"></div>
+        <div className="fixed inset-0 bg-center mix-blend-color-dodge ooorganize"></div>
+      </div>
     </div>
   );
 }
